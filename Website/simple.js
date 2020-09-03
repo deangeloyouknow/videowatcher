da838959a0e7d794be70826ad797b5aeafeabaa3
deangeloyouknow <deangeloyouknow@gmail.com>
2020-09-02 20:20:53 -0700

    Add files via upload
    I added video player functionality

--- Website/simple.js
+++ Website/simple.js
@@ -1,29 +1,29 @@
-function show(){
+function show() {
 
   alert('you clicked this');
 }
 
 
-function doCelsius(c){
+function doCelsius(c) {
 
-  return (9/5) * c + 32; // The reason why it wasn't working becasue i forgot the multiplaction symbol //
+  return (9 / 5) * c + 32; // The reason why it wasn't working becasue i forgot the multiplaction symbol //
 
 }
 
 
-function showFar(){
+function showFar() {
   var farcal = doCelsius(0);
   alert(farcal);
 }
 
-function showNewFar(){
+function showNewFar() {
 
   var dofar = doCelsius(0);
   alert(dofar);
 }
 
 
-function cel_100(){
+function cel_100() {
 
   var celdo100 = doCelsius(100);
   alert(celdo100);
